544bc4a7cf54d1d3d21546cff06a5549a3223802
Kkalla <dkswns333@gmail.com>
2017-04-28 14:28:42 +0900

    Complete problem #2 fractional_knapsack
     -Insert code for compute value

--- Algorithmic Toolbox/greedy_algorithms/02_greedy_algorithms_starter_files/fractional_knapsack/fractional_knapsack.cpp
+++ Algorithmic Toolbox/greedy_algorithms/02_greedy_algorithms_starter_files/fractional_knapsack/fractional_knapsack.cpp
@@ -10,9 +10,19 @@ double get_optimal_value(int capacity, vector<int> weights, vector<int> values) 
   while(n){
 	if(capacity==0)
 		break;
-	for(int i=0;i<n;++i){
-		
+	int i=0;
+	double max = 0.0;
+	for(int j=0;j<n;++j){
+		if(weights[j]>0&&max<(values[j]/weights[j])){
+			max = static_cast<double>(values[j])/weights[j];
+			i = j;
+		}
 	}
+	int a = std::min(weights[i],capacity);
+	value = value + a*(static_cast<double>(values[i])/weights[i]);
+	weights[i] -= a;
+	capacity -= a;
+	n--;
   }
 
   return value;
